7cf46e1ba6d67511450de6c945809fae91d19463
Sediqa <sadat.hadid2000@gmail.com>
2022-07-05 14:30:58 +0000

    Store words and hints as object in wordList.js file

---
+++ assets/js/wordList.js
@@ -0,0 +1,51 @@
+const wordDetails = [
+  
+    {
+        word: "google",
+        hint: "search engin"
+    },
+    {
+        word: "earth",
+        hint: "planet of our solar system  "
+    },
+    {
+        word: "mysql",
+        hint: "relational database system"
+    },
+    {
+        word: "pizza",
+        hint: "italian food "
+    },
+    {
+        word: "piano",
+        hint: "Musical instruments "
+    },
+    {
+        word: "Toronto",
+        hint: "Famous city of Canada "
+    },
+    {
+        word: "Cola ",
+        hint: "kind of drinks "
+    },
+    {
+        word: "server",
+        hint: "related to computer and system "
+    },
+    {
+        word: "instagram",
+        hint: "popular social media "
+    },
+    {
+        word: "python",
+        hint: "programming language "
+    },
+    {
+        word: "computer",
+        hint: "electronic device "
+    },
+    {
+        word: "github",
+        hint: "code hosting platfrom "
+    }
+]

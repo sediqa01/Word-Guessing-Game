ff576604ac9f01295d31171b4db6d75a2f89c187
Sediqa <sadat.hadid2000@gmail.com>
2022-07-05 17:04:36 +0000

    Creat inputs tag according to the words length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
+const inputs = document.querySelector(".game-inputs");
+
 function randomWords() {
     //getting random object from wordList
     let objFirst = wordDetails[Math.floor(Math.random() * wordDetails.length)];
     let word = objFirst.word;  //getting word from random object
     console.log(word);
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        
+        html += `<input type="text" disabled>`;
+    
+    }
+    inputs.innerHTML = html;
 }
 randomWords();

b2efb220a397ebecb0befc1dd661d916800890e8
Sediqa <sadat.hadid2000@gmail.com>
2022-07-06 14:51:26 +0000

    Get automatically focusing input when user pressed any key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
 const inputs = document.querySelector(".game-inputs"),
   resetBtn = document.querySelector(".btn"),
-  hint = document.querySelector(".hint span");
+  hint = document.querySelector(".hint span"),
+  typingInput = document.querySelector(".typing");
 
 function randomWords() {
-    //getting random object from wordList
-    let objFirst = wordDetails[Math.floor(Math.random() * wordDetails.length)];
-    let word = objFirst.word;  //getting word from random object
-    console.log(word);
+  //getting random object from wordList
+  let objFirst = wordDetails[Math.floor(Math.random() * wordDetails.length)];
+  let word = objFirst.word; //getting word from random object
+  console.log(word);
 
-    hint.innerHTML = objFirst.hint;
+  hint.innerHTML = objFirst.hint;
 
-    let html = "";
-    for (let i = 0; i < word.length; i++) {
-        
-        html += `<input type="text" disabled>`;
-    
-    }
-    inputs.innerHTML = html;
+  let html = "";
+  for (let i = 0; i < word.length; i++) {
+
+    html += `<input type="text" disabled>`;
+
+  }
+  inputs.innerHTML = html;
 }
 randomWords();
 
-resetBtn.addEventListener("click" , randomWords);
+resetBtn.addEventListener("click", randomWords);
+document.addEventListener("keydown", () => typingInput.focus());
